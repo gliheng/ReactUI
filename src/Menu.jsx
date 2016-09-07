e6a7812173e480df20195f4b6f43ea47cc240592
meiyajun <meiyajun@oa.gf.com.cn>
2016-09-07 19:43:15 +0800

    update menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -5,6 +5,7 @@ import {getTMPDOMRoot, extend} from './Utils';
 
 const SEP = '__seperator__';
 let Menu = React.createClass({
+
     getInitialState() {
         return {
             show: false
@@ -34,6 +35,11 @@ let Menu = React.createClass({
             left += 2;
         }
 
+        var allShow = true;
+        var allItem = 0;
+        var visibleItem = 0;
+        var firstItem = 0;
+
         var options = config ? config.options : this.props.options,
             menuHeight = options.length * Constants.config.menuItemHeight;
         if (docTop + top + menuHeight > document.body.scrollTop + window.innerHeight) {
@@ -41,11 +47,26 @@ let Menu = React.createClass({
             var submenuSize = options.filter((opt)=> opt != SEP).length;
             var menuHeight = (this.isRootMenu() ? submenuSize : submenuSize - 1) * Constants.config.menuItemHeight
                 + (options.length - submenuSize) * Constants.config.menuSepItemHeight;
-            top -= menuHeight;
+            //top -= menuHeight;
+
+            if(top-menuHeight < 0) {
+                allShow = false;
+                allItem = submenuSize;
+                visibleItem = Math.floor((top-30)/(Constants.config.menuItemHeight+1));
+                firstItem = 0;
+                top = 2;
+            }
+            else {
+                top -= menuHeight;
+            }
         }
 
         this.setState(extend({
             show: true,
+            allShow: allShow,
+            allItem: allItem,
+            visibleItem: visibleItem,
+            firstItem: firstItem,
             position: {
                 top: top,
                 left: left
@@ -68,6 +89,22 @@ let Menu = React.createClass({
             var parent = this.props.parent;
             parent && parent.hide(all);
         }
+    },
+
+    onClickArrow(evt) {
+        evt.stopPropagation();
+        var idx = parseInt(evt.currentTarget.dataset['idx']);
+        var firstItem = this.state.firstItem;
+        var visibleItem = this.state.visibleItem;
+        var allItem = this.state.allItem;
+        if(idx==1 && firstItem>0)
+            firstItem--;
+        if(idx==2 && firstItem+visibleItem<allItem)
+            firstItem++;
+
+        this.setState({
+            firstItem: firstItem
+        });
     },
 
     onClick(evt) {
@@ -127,7 +164,7 @@ let Menu = React.createClass({
             subMenu;
 
         if (label == SEP) {
-            return [<li className="sep" />, idx];
+            return [<li className="sep" />, ++idx];
         }
 
         if (typeof option == 'object') {
@@ -142,7 +179,7 @@ let Menu = React.createClass({
         }
 
         var item = <li
-            className={chose?"on":""}
+            className={chose ? "on" : ""}
             key={idx}
             style={style}
             data-idx={idx}
@@ -156,15 +193,71 @@ let Menu = React.createClass({
     render() {
         var className = 'Menu ' + (this.state.show ? 'Show' : 'Hide'),
             options = this.state.options || this.props.options,
-            position = this.state.position || this.props.position,
-            contents;
+            position = this.state.position || this.props.position;
+        var items = [];
+        var contents;
+        var state = this.state;
         if (options) {
+            var arrowT,arrowB;
+           // var icoT = (state.firstItem<=0?''<i className="ico top"></i>);
+            if(!state.allShow) {
+                arrowT = state.firstItem<=0?(<li className="arrow"></li>):(<li className="arrow" data-idx='1' onClick={this.onClickArrow}><i className="ico top"></i></li>);
+                arrowB = (state.firstItem+state.visibleItem)>=state.allItem?(<li className="arrow"></li>):(<li className="arrow" data-idx='2' onClick={this.onClickArrow}><i className="ico bottom"></i></li>);
+            }
             var idx = 0, item;
-            var items = options.map((item)=> {
+            var itemIdx = 0,itemNum = 0;
+
+            if(!state.allShow) items.push(arrowT);
+
+            for(var idx=0;idx<options.length;idx++) {
+                var option = options[idx];
+                var label = option,
+                    style,
+                    chose,
+                    subMenu;
+                if(label!=SEP) itemIdx++;
+                if(!state.allShow && itemIdx<=state.firstItem)
+                    continue;
+
+                if (label == SEP) {
+                    item = (<li className="sep" />);
+                    items.push(item);
+                    continue;
+                }
+
+                if (typeof option == 'object') {
+                    label = [option.title];
+                    chose = option.chose;
+                    if (Array.isArray(option.children)) {
+                        label.push(<i className="ico gt"></i>);
+                        subMenu = <Menu ref={`child-${idx}`} idx={idx} options={option.children} parent={this}/>;
+                    }
+                    style = option.style;
+                }
+
+                item = (<li
+                    className={chose ? "on" : ""}
+                    key={idx}
+                    style={style}
+                    data-idx={idx}
+                    onMouseEnter={this.onHoverMenuItem}
+                    onMouseLeave={this.onLeaveMenuItem}
+                    onClick={this.onClick}>{label}{subMenu}</li>);
+
+                itemNum++;
+                items.push(item);
+
+                if(!state.allShow && itemNum>=state.visibleItem)
+                    break;
+            }
+
+            if(!state.allShow) items.push(arrowB);
+
+            /*var items = options.map((item)=> {
                 // For seperators, index should not increase
                 [item, idx] = this.renderMenuItem(item, idx);
                 return item;
-            });
+            });*/
 
             contents = (
                 <div className="list">
@@ -193,6 +286,7 @@ export default {
         var menu = <Menu options={options}/>;
         this.menu = React.render(menu, $root);
         this.hide = this.hide.bind(this);
+        this.click = this.click.bind(this);
     },
 
     show: function (evt, config, cbk) {
@@ -220,16 +314,31 @@ export default {
             cbk: (...args)=> {
                 // this is triggered when menu item is clicked
                 cbk.apply(null, args);
-                document.removeEventListener('click', this.hide);
+                document.removeEventListener('click', this.click);
             }
         });
-        document.addEventListener('click', this.hide);
+        document.addEventListener('click', this.click);
+    },
+
+    click(evt) {
+        if (!this.menu) return;
+        var $menunode =  this.menu.getDOMNode();
+        var x = document.body.scrollLeft + evt.clientX,
+            y = document.body.scrollTop + evt.clientY;
+
+        var menux = $menunode.offsetLeft,
+            menuy = $menunode.offsetTop;
+        var menuw = $menunode.offsetWidth,
+            menuh = $menunode.offsetHeight;
+        
+        if(x<menux || x>(menux+menuw) || y<menuy || y>(menuy+menuh))
+            this.hide();
     },
 
     hide() {
         if (!this.menu) return;
         this.menu.hide();
-        document.removeEventListener('click', this.hide);
+        document.removeEventListener('click', this.click);
 
         var $root = this.menu.getDOMNode().parentNode;
         React.unmountComponentAtNode($root);
